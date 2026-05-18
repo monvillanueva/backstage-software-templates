c111a73b491b819e863ea69c9fa58c5972f1375d
monica-villanueva <63138869+monica-villanueva@users.noreply.github.com>
2026-05-18 13:40:36 -0700

    copy python-app

---
+++ python-app/template/src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify(
+        {
+            'time': str(datetime.now().isoformat()),
+            'hostname': socket.gethostname(),
+            'message': 'You are doing great, human! Keep going! You can do it!',
+            'deployed_on': "kubernetes"
+        }
+    )
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+            'status': 'up'
+        }), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+#'/api/v1/details'
+#'/api/v1/healthz'
